1f578839a336fba95bb818c2de28ad1bd1caf883
Kyle Stewart <kstew1017@outlook.com>
2023-07-13 17:18:50 -0500

    implmented redux for state management

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,7 @@
 import { ScrollView } from 'react-native';
 import { Card, Text, Avatar, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 const Mission = () => {
     return (
@@ -18,7 +18,7 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-    const [partners, setPartners] = useState(PARTNERS);
+    const partners = useSelector((state) => state.partners);
 
     return (
         <ScrollView>
@@ -26,9 +26,9 @@ const AboutScreen = () => {
             <Card title={'Community Partners'}>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.map((partner) => (
+                {partners.partnersArray.map((partner) => (
                         <ListItem key={partner.id}>
-                            <Avatar rounded source={partner.image} />
+                            <Avatar rounded source={{ uri: baseUrl + partner.image}} />
                             <ListItem.Content>
                                 <ListItem.Title>{partner.name}</ListItem.Title>
                                 <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
